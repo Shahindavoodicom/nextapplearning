0065cb1e3a34128e0f247e17020cc3191eff83d7
shahin davoodi <shahindavoodi1379@gmail.com>
2024-07-06 17:31:04 +0330

    fetch server side

--- app/jokes/page.tsx
+++ app/jokes/page.tsx
@@ -1,4 +1,14 @@
-import { Jokes } from "@/components/jokes";
-export default function Page() {
-  return <Jokes />;
+async function getData() {
+  const res = await fetch("https://api.chucknorris.io/jokes/random");
+  if (!res.ok) {
+    console.log('fetch failed');
+    
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const joke = await getData();
+  return <h2>{joke.value}</h2>;
 }
